cbb62ca85b5cc0b3181fc0e18b0e82c57c941813
Kevin Choubacha <kchoubacha@weedmaps.com>
2017-11-30 17:49:27 -0800

    Use all "files" found in args after first

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::env;
 use glob::glob;
 use regex::Regex;
 
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let regex = &args[1];
@@ -14,22 +15,24 @@ fn main() {
     if let Err(parse_error) = regex {
         println!("{}", parse_error);
         std::process::exit(1);
-        return;
     }
     let regex = regex.unwrap();
 
-    let query = if args.len() > 2 { &args[2] } else { "." };
-    let files = glob(query).expect("Glob pattern failed");
-    files
-        .filter(|p| p.is_ok())
-        .map(|p| p.unwrap())
-        .map(|p| matcher::find_matches(p.as_path(), &regex).expect("Could not parse file"))
-        .filter(|matches| matches.has_matches())
-        .for_each(|matches|{
-            println!("{} matched {} times", matches.path.as_path().to_str().unwrap(), matches.count);
-            for m in matches.matches {
-                println!("{}:{}", m.number, m.line.trim_right());
-            }
-            println!();
-        })
+    let default_search = [".".to_string()];
+    let queries = if args.len() > 2 { &args[2..] } else { &default_search };
+    for query in queries {
+        let files = glob(query).expect("Glob pattern failed");
+        files
+            .filter(|p| p.is_ok())
+            .map(|p| p.unwrap())
+            .map(|p| matcher::find_matches(p.as_path(), &regex).expect("Could not parse file"))
+            .filter(|matches| matches.has_matches())
+            .for_each(|matches| {
+                println!("{} matched {} times", matches.path.as_path().to_str().unwrap(), matches.count);
+                for m in matches.matches {
+                    println!("{}:{}", m.number, m.line.trim_right());
+                }
+                println!();
+            })
+    }
 }
