da961e52e2b35f44927a4949d4d21785798999ee
Kevin Choubacha <kchoubacha@weedmaps.com>
2017-11-30 19:59:52 -0800

    Parse params with clap

--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,56 @@
-use clap::{Arg, App};
+use clap::ArgMatches;
+
+use regex;
+use regex::{Regex};
+use std;
 
 pub struct Opts {
+    pub regex: Regex,
+    pub queries: Vec<String>,
 }
 
-fn load_args() {
+#[derive(Debug)]
+pub enum ArgError {
+    InvalidRegex(String),
+    Incomplete,
+}
+
+fn get_regex(regex: &str) -> Result<Regex, ArgError> {
+    let regex = match Regex::new(&regex) {
+        Ok(regex) => regex,
+        Err(regex::Error::Syntax(msg)) => return Err(ArgError::InvalidRegex(msg)),
+        Err(regex::Error::CompiledTooBig(_)) => return Err(ArgError::InvalidRegex("Regex too large".to_string())),
+        Err(_) => return Err(ArgError::InvalidRegex("Unknown regex parsing error".to_string())),
+    };
+    Ok(regex)
+}
+
+pub fn get_opts() -> Result<Opts, ArgError> {
+    let matches = clap_app!(myapp =>
+        (author: "Kevin C. <chewbacha@gmail.com>")
+        (about: "Searches with regex through files. For fun!")
+        (@arg REGEX: +required "The pattern that should be matched")
+        (@arg PATTERN: ... "The files to search")
+    ).get_matches();
+
+    let regex = matches.value_of("REGEX").expect("Regex required!");
+    let queries = matches.values_of("PATTERN").unwrap().map(|p| p.to_owned()).collect();
+    Ok(Opts { regex: get_regex(regex)?, queries: queries })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_parses_into_a_regex() {
+        let regex = get_regex("test").unwrap();
+        assert!(regex.is_match("test"));
+    }
+
+    #[test]
+    fn it_errors_when_parsing_bad_regex() {
+        let result = get_regex("test(");
+        assert!(result.is_err());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
+#[macro_use]
 extern crate clap;
 extern crate glob;
 extern crate regex;
 
 mod matcher;
+mod args;
 use std::env;
 use glob::glob;
 use regex::Regex;
 
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let regex = &args[1];
-    let regex = Regex::new(regex);
-    if let Err(parse_error) = regex {
-        println!("{}", parse_error);
-        std::process::exit(1);
-    }
-    let regex = regex.unwrap();
-
-    let default_search = [".".to_string()];
-    let queries = if args.len() > 2 { &args[2..] } else { &default_search };
-    for query in queries {
-        let files = glob(query).expect("Glob pattern failed");
+    let opts = match args::get_opts() {
+        Ok(o) => o,
+        Err(e) => {
+            println!("{:?}", e);
+            std::process::exit(1);
+        }
+    };
+    for query in &opts.queries {
+        let files = glob(&query).expect("Glob pattern failed");
         files
             .filter(|p| p.is_ok())
             .map(|p| p.unwrap())
-            .map(|p| matcher::find_matches(p.as_path(), &regex).expect("Could not parse file"))
+            .map(|p| matcher::find_matches(p.as_path(), &opts.regex).expect("Could not parse file"))
             .filter(|matches| matches.has_matches())
             .for_each(|matches| {
                 println!("{} matched {} times", matches.path.as_path().to_str().unwrap(), matches.count);
