0a913c1710448af13439eb17a4c87584111b4189
Kevin Choubacha <kchoubacha@weedmaps.com>
2017-12-04 21:52:23 -0800

    Add recursion to the file searching

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate regex;
 mod matcher;
 mod args;
 mod display;
+mod files;
 use glob::glob;
 
 fn main() {
@@ -17,13 +18,18 @@ fn main() {
         }
     };
     for query in &opts.queries {
-        let files = glob(&query).expect("Glob pattern failed");
-        files
+        let files = glob(&query)
+            .expect("Glob pattern failed")
             .filter(|p| p.is_ok())
-            .map(|p| p.unwrap())
-            .map(|p| matcher::find_matches(p.as_path(), &opts.regex).expect("Could not parse file"))
-            .filter(|m| m.has_matches())
-            .map(|m| display::MatchDisplay::new(m))
-            .for_each(|m| println!("{}", m));
+            .map(|p| p.expect("An 'ok' file was not found"));
+        for file in files {
+            let sub_files = files::recurse(file).expect("Could not find files");
+            sub_files
+                .into_iter()
+                .map(|p| matcher::find_matches(p.as_path(), &opts.regex).expect("Could not parse file"))
+                .filter(|m| m.has_matches())
+                .map(|m| display::MatchDisplay::new(m))
+                .for_each(|m| println!("{}", m));
+        }
     }
 }
