6e0f3b8c976987440f5b5ecd6af33fc2875fc849
Charles King <bringking@gmail.com>
2017-12-05 10:15:56 -0700

    Working implementation with capture colors

--- src/display.rs
+++ src/display.rs
@@ -12,6 +12,21 @@ pub struct MatchesDisplay {
 }
 
 impl<'a> MatchDisplay<'a> {
+    fn prefix_fmt(&self) -> ColoredString {
+        self.match_to_display.number.to_string().bright_yellow()
+    }
+
+    fn line_fmt(&self) -> String {
+        let mut output =String::new();
+
+        for cap in &self.match_to_display.captures {
+
+            output.push_str(&self.match_to_display.line.replace(cap, &cap.black().on_yellow().to_string()))
+        }
+
+        output.trim_right().to_string()
+    }
+
     pub fn new(match_to_display: &'a Match) -> MatchDisplay {
         MatchDisplay { match_to_display: match_to_display }
     }
@@ -25,7 +40,7 @@ impl MatchesDisplay {
 
 impl<'a> fmt::Display for MatchDisplay<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "{}:{}", self.match_to_display.number.to_string().bright_yellow(), self.match_to_display.line.trim_right())?;
+        writeln!(f, "{}:{}", self.prefix_fmt(), self.line_fmt())?;
         Ok(())
     }
 }
@@ -34,7 +49,7 @@ impl fmt::Display for MatchesDisplay {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "{} matched {} times",
                  self.matches.path.as_path().to_str().unwrap().bright_green(),
-                 self.matches.count.to_string().bright_yellow())?;
+                 self.matches.count.to_string().yellow())?;
 
         for m in &self.matches.matches {
             write!(f, "{}", MatchDisplay::new(m));
@@ -56,11 +71,16 @@ mod tests {
         let m = Matches {
             count: 12,
             path: Path::new("./path/to/something").to_owned(),
-            matches: vec![Match { number: 23, line: "some text line".to_string() }],
+            matches: vec![Match { number: 23, line: "some text line".to_string(), captures: vec!["text".to_string()]  }],
         };
         assert_eq!(
-            format!("{}", MatchDisplay::new(m)),
-            "./path/to/something matched 12 times\n23:some text line\n"
+            format!("{}", MatchesDisplay::new(m)),
+            format!("{path} matched {count} times\n{line_number}:some {capture} line\n",
+                    path = "./path/to/something".to_string().bright_green(),
+                    count = 12.to_string().yellow(),
+                    line_number = 23.to_string().yellow(),
+                    capture = "text".to_string().black().on_yellow()
+            )
         )
     }
 }
