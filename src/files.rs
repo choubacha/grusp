84395a33f8da7b68300fa469c30ad4798c29b083
Kevin Choubacha <kchoubacha@weedmaps.com>
2017-12-05 22:09:11 -0800

    Implement threading using rayon

--- src/files.rs
+++ src/files.rs
@@ -1,17 +1,11 @@
 use std::path::PathBuf;
 use std::io::Result;
 
-pub fn recurse(path: PathBuf) -> Result<Vec<PathBuf>>{
-    let mut files = Vec::new();
-    recurse_down(path, &mut files)?;
-    Ok(files)
-}
-
-fn recurse_down(path: PathBuf, files: &mut Vec<PathBuf>) -> Result<()> {
+pub fn recurse(path: PathBuf, files: &mut Vec<PathBuf>) -> Result<()> {
     if path.is_dir() {
         let entries = path.read_dir()?;
         for entry in entries {
-            recurse_down(entry?.path(), files)?
+            recurse(entry?.path(), files)?
         }
         Ok(())
     } else {
@@ -28,7 +22,8 @@ mod tests {
     #[test]
     fn it_finds_files_inside_directory_if_path_is_dir() {
         let path = Path::new("./example_dir/sub_dir");
-        let files = recurse(path.to_owned()).unwrap();
+        let mut files = Vec::new();
+        recurse(path.to_owned(), &mut files).unwrap();
         assert_eq!(files.len(), 2);
         assert!(files.contains(&Path::new("./example_dir/sub_dir/sub-example-1.txt").to_owned()));
         assert!(!files.contains(&Path::new("./example_dir/example-1.txt").to_owned()));
@@ -37,7 +32,8 @@ mod tests {
     #[test]
     fn it_finds_files_inside_sub_directories() {
         let path = Path::new("./example_dir");
-        let files = recurse(path.to_owned()).unwrap();
+        let mut files = Vec::new();
+        recurse(path.to_owned(), &mut files).unwrap();
         assert_eq!(files.len(), 4);
         assert!(files.contains(&Path::new("./example_dir/sub_dir/sub-example-1.txt").to_owned()));
         assert!(files.contains(&Path::new("./example_dir/example-1.txt").to_owned()));
@@ -46,7 +42,8 @@ mod tests {
     #[test]
     fn it_returns_current_file_if_a_file_is_passed_in() {
         let path = Path::new("./example_dir/example-1.txt");
-        let files = recurse(path.to_owned()).unwrap();
+        let mut files = Vec::new();
+        recurse(path.to_owned(), &mut files).unwrap();
         assert_eq!(files.len(), 1);
         assert_eq!(files, &[path]);
     }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 extern crate clap;
 extern crate glob;
 extern crate regex;
+extern crate rayon;
 
 mod matcher;
 mod args;
 mod display;
 mod files;
 use glob::glob;
+use rayon::prelude::*;
+use std::path::PathBuf;
 
 fn main() {
     let opts = match args::get_opts() {
@@ -17,19 +20,25 @@ fn main() {
             std::process::exit(1);
         }
     };
+    let mut files = Vec::new();
     for query in &opts.queries {
-        let files = glob(&query)
+        glob(&query)
             .expect("Glob pattern failed")
             .filter(|p| p.is_ok())
-            .map(|p| p.expect("An 'ok' file was not found"));
-        for file in files {
-            let sub_files = files::recurse(file).expect("Could not find files");
-            sub_files
-                .into_iter()
-                .map(|p| matcher::find_matches(p.as_path(), &opts.regex).expect("Could not parse file"))
-                .filter(|m| m.has_matches())
-                .map(|m| display::MatchDisplay::new(m))
-                .for_each(|m| println!("{}", m));
-        }
+            .map(|p| p.expect("An 'ok' file was not found"))
+            .for_each(|p| files::recurse(p, &mut files).expect("Unknown file error"));
+    }
+
+    if opts.is_concurrent {
+        files.into_par_iter().for_each(|p| match_file(p, &opts));
+    } else {
+        files.into_iter().for_each(|p| match_file(p, &opts));
+    };
+}
+
+fn match_file(path: PathBuf, opts: &args::Opts) {
+    let matches = matcher::find_matches(path.as_path(), &opts.regex).expect("Could not parse file");
+    if matches.has_matches() {
+        println!("{}", display::MatchDisplay::new(matches));
     }
 }
